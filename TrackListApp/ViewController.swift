24251989ede8bf81087afa7148d903efcf094136
Nikkru <56563372+Nikkru@users.noreply.github.com>
2020-06-08 20:48:45 +0300

    Initial Commit

---
+++ TrackListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrackListApp
+//
+//  Created by Nikolai Krusser on 08.06.2020.
+//  Copyright © 2020 Nikolai Krusser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
